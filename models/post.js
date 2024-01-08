4f675e67ff72175a8a09f19ac3b41f5f90021e22
Haddock <jhadddock@protonmail.com>
2024-01-07 16:24:13 -0800

    Define Post Schema

---
+++ models/post.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  date_created: { type: Date },
+  message: { type: String, required: true, maxLength: 60000 },
+  title: { type: String, required: true, maxLength: 10000 },
+});
+
+module.exports = mongoose.model("Post", PostSchema);
