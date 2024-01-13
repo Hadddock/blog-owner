bb762d840c317890f23f9ae76233f9617cc3ddfc
Haddock <jhadddock@protonmail.com>
2024-01-12 18:07:25 -0800

    Add min constraints to post message and title

--- models/post.js
+++ models/post.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User" },
   date_created: { type: Date },
-  message: { type: String, required: true, maxLength: 60000 },
-  title: { type: String, required: true, maxLength: 10000 },
-  publish_date: { type: Date },
+  message: { type: String, required: true, maxLength: 60000, minLength: 1 },
+  title: { type: String, required: true, maxLength: 10000, minLength: 1 },
+  publish_date: { type: Date, default: new Date() },
 });
 
 module.exports = mongoose.model("Post", PostSchema);
