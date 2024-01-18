84cfba972ae1eacd4f6d0fc5745c072bc91ac437
Haddock <jhadddock@protonmail.com>
2024-01-17 16:14:17 -0800

    Add controllers & endpoints for authentication

--- controller/userController.js
+++ controller/userController.js
@@ -2,6 +2,7 @@ const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
 const User = require("./../models/user");
+const passport = require("passport");
 
 exports.sign_up_get = asyncHandler(async (req, res, next) => {
   res.render("sign-up");
@@ -48,3 +49,43 @@ exports.sign_up_post = [
     }
   }),
 ];
+
+exports.login_get = asyncHandler(async (req, res, next) => {
+  res.render("login");
+});
+
+// exports.login_post = passport.authenticate("local", {
+//   successRedirect: "/",
+//   failureRedirect: "/login",
+//   failureMessage: true,
+// });
+
+exports.logout_post = asyncHandler(async (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+exports.login_post = asyncHandler(async (req, res) => {
+  passport.authenticate("local", (err, user, options) => {
+    if (user) {
+      req.login(user, (error) => {
+        if (error) {
+          res.send(error);
+        } else {
+          console.log("Successfully authenticated");
+          res.redirect("/");
+        }
+      });
+    } else {
+      res.render("login", {
+        errors: [
+          { msg: "Failed to login, check username and password are correct" },
+        ],
+      });
+    }
+  })(req, res);
+});
--- routes/catalog.js
+++ routes/catalog.js
@@ -7,6 +7,12 @@ const post_controller = require("../controller/postController");
 router.get("/sign-up", user_controller.sign_up_get);
 router.post("/sign-up", user_controller.sign_up_post);
 
+router.get("/login", user_controller.login_get);
+
+router.post("/login", user_controller.login_post);
+
+router.post("/logout", user_controller.logout_post);
+
 router.get("/post", post_controller.post_get);
 router.post("/post", post_controller.post_post);
 
