6933160121b7aa051052ada7341565d18e3ed192
Haddock <jhadddock@protonmail.com>
2024-01-23 17:12:30 -0800

    Display posts on index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const Post = require("./../models/post");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", async function (req, res, next) {
+  const posts = await Post.find({ publish_date: { $lte: new Date() } })
+    .sort({ publish_date: -1 })
+    .exec();
+  res.render("index", { title: "Express", posts: posts });
 });
 
 module.exports = router;
