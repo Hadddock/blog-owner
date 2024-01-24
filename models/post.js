de1e266f5787b23e9f1b4a071cf15e61ce691be2
Haddock <jhadddock@protonmail.com>
2024-01-23 17:10:37 -0800

    Add edit date to catalog model

--- models/post.js
+++ models/post.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User" },
   date_created: { type: Date },
+  edit_date: { type: Date },
   message: { type: String, required: true, maxLength: 60000, minLength: 1 },
   title: { type: String, required: true, maxLength: 10000, minLength: 1 },
   publish_date: { type: Date, default: new Date() },
