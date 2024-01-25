6a318a43b622b3707969ab8679986e6b4f4c991c
Haddock <jhadddock@protonmail.com>
2024-01-24 18:38:28 -0800

    Add route for commenting on post

--- routes/catalog.js
+++ routes/catalog.js
@@ -7,7 +7,8 @@ const post_controller = require("../controller/postController");
 router.get("/sign-up", user_controller.sign_up_get);
 router.post("/sign-up", user_controller.sign_up_post);
 
-router.get("/post/:id", post_controller.post_detail);
+router.get("/post/:id", post_controller.post_detail_get);
+router.post("/post/:id", post_controller.post_detail_post);
 
 router.get("/login", user_controller.login_get);
 
