c73fd149c4b82bd92e134736c6490057327698d4
Haddock <jhadddock@protonmail.com>
2024-01-24 18:38:17 -0800

    Add controller to comment on post

--- controller/postController.js
+++ controller/postController.js
@@ -1,17 +1,21 @@
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 const Post = require("./../models/post");
+const Comment = require("./../models/comment");
 
 exports.post_get = asyncHandler(async (req, res, next) => {
   res.render("post");
 });
 
-exports.post_detail = asyncHandler(async (req, res, next) => {
+exports.post_detail_get = asyncHandler(async (req, res, next) => {
   if (req.params.id) {
     const post = await Post.findOne({ _id: req.params.id }).exec();
+    const comments = await Comment.find({ post: req.params.id })
+      .populate("user")
+      .exec();
     const currentDate = new Date();
     if (post.publish_date === null || post.publish_date <= currentDate) {
-      res.render("post-detail", { post: post });
+      res.render("post-detail", { post: post, comments: comments });
     } else {
       res.sendStatus(404);
     }
@@ -19,6 +23,42 @@ exports.post_detail = asyncHandler(async (req, res, next) => {
     res.sendStatus(404);
   }
 });
+
+exports.post_detail_post = [
+  body("message")
+    .trim()
+    .isLength({ min: 1, max: 60000 })
+    .withMessage("Comment must be 1-60000 characters long")
+    .escape(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (req.params.id && req.user) {
+      const post = await Post.findOne({ _id: req.params.id }).exec();
+
+      const comment = new Comment({
+        message: req.body.message,
+        post: req.params.id,
+        user: req.user._id,
+        date_created: new Date(),
+      });
+
+      if (!errors.isEmpty()) {
+        res.render("post-detail", {
+          post: post,
+          comment: comment,
+          errors: errors.array(),
+        });
+        return;
+      } else {
+        await comment.save();
+      }
+
+      res.redirect("/post/" + req.params.id);
+    } else {
+      res.sendStatus(404);
+    }
+  }),
+];
 
 exports.post_post = [
   body("title")
