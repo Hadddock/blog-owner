80c935cb52974c04d68585cebfb0124a0927a08a
Haddock <jhadddock@protonmail.com>
2024-01-25 16:16:43 -0800

    Show unpusblished posts in post list

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,7 @@ const Post = require("./../models/post");
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-  const posts = await Post.find({ publish_date: { $lte: new Date() } })
-    .sort({ publish_date: -1 })
-    .exec();
+  const posts = await Post.find().sort({ publish_date: -1 }).exec();
   res.render("index", { title: "Express", posts: posts });
 });
 
